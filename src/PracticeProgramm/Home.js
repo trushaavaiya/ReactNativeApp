5212499a28be6225b13fa6f38bd81978c054d7e5
Trusha Avaiya <trushaavaiya@gmail.com>
2025-05-20 10:43:32 +0530

    basic

---
+++ src/PracticeProgramm/Home.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+export default function App(){
+
+    return(
+
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
