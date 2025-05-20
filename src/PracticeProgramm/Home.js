440d14d14fe3f5e105fc2f1e664f2d806b48ad6d
Trusha Avaiya <trushaavaiya@gmail.com>
2025-05-20 10:49:18 +0530

    return statement

--- src/PracticeProgramm/Home.js
+++ src/PracticeProgramm/Home.js
@@ -6,7 +6,32 @@ import Icon from 'react-native-vector-icons/Ionicons';
 export default function App(){
 
     return(
-        <View>
+        <View style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>{renderScreen()}</View>
+
+      
+      <View style={styles.bottomBar}>
+        <TouchableOpacity onPress={() => setSelected('Home')} style={styles.iconButton}>
+          <Icon name={selected === 'Home' ? 'home' : 'home-outline'} size={30} color={selected === 'Home' ? 'tomato' : 'gray'} />
+          <Text style={{ color: selected === 'Home' ? 'tomato' : 'gray' }}>Home</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => setSelected('Orders')} style={styles.iconButton}>
+          <Icon name={selected === 'Orders' ? 'list' : 'list-outline'} size={30} color={selected === 'Orders' ? 'tomato' : 'gray'} />
+          <Text style={{ color: selected === 'Orders' ? 'tomato' : 'gray' }}>Orders</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => setSelected('OrderDetails')} style={styles.iconButton}>
+          <Icon name={selected === 'OrderDetails' ? 'document' : 'document-outline'} size={30} color={selected === 'OrderDetails' ? 'tomato' : 'gray'} />
+          <Text style={{ color: selected === 'OrderDetails' ? 'tomato' : 'gray' }}>Details</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => setSelected('Wallet')} style={styles.iconButton}>
+          <Icon name={selected === 'Wallet' ? 'wallet' : 'wallet-outline'} size={30} color={selected === 'Wallet' ? 'tomato' : 'gray'} />
+          <Text style={{ color: selected === 'Wallet' ? 'tomato' : 'gray' }}>Wallet</Text>
+        </TouchableOpacity>
+      </View>
+
             
         </View>
     );
